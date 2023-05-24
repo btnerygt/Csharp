6e0699f033d618b4627bb0f6bb1c79029a3961c2
Batın Eryiğit <btnerygt1995@gmail.com>
2023-05-24 16:58:21 +0300

    Add files via upload

---
+++ basit_hesap_makinesi/basit_hesap_makinesi/Program.cs
@@ -0,0 +1,36 @@
+﻿int sonuc = 0;
+
+Console.WriteLine("Toplama İşlemi İçin +");
+Console.WriteLine("Çıkarma İşlemi İçin -");
+Console.WriteLine("Çarpma İşlemi İçin *");
+Console.WriteLine("Bölme İşlemi İçin /");
+Console.Write("Yapmak İstediğiniz işlemi Seçin : ");
+char secim = Convert.ToChar(Console.ReadLine());
+
+Console.Write("1.değeri girin : ");
+int sayi1 = Convert.ToInt16(Console.ReadLine());
+Console.Write("2.değeri girin : ");
+int sayi2 = Convert.ToInt16(Console.ReadLine());
+
+switch (secim)
+{
+    case '+':
+        sonuc = sayi1 + sayi2;
+        Console.WriteLine("Toplama işlemini sonucu : {0}", sonuc);
+        break;
+    case '-':
+        sonuc = sayi1 - sayi2;
+        Console.WriteLine("Çıkarma işleminin sonucu : {0}", sonuc);
+        break;
+    case '*':
+        sonuc = sayi1 * sayi2;
+        Console.WriteLine("Çıkarma işleminin sonucu : {0}", sonuc);
+        break;
+    case '/':
+        sonuc = sayi1 / sayi2;
+        Console.WriteLine("Çıkarma işleminin sonucu : {0}", sonuc);
+        break;
+    default:
+        Console.WriteLine("Hatalı Seçim Yaptınız");
+        break;
+}
---
+++ daire_alan_cevre/daire_alan_cevre/Program.cs
@@ -0,0 +1,10 @@
+﻿double pi = 3.14, daireAlan, daireCevre, yariCap;
+
+Console.Write("Yaricapi giriniz : ");
+yariCap = double.Parse(Console.ReadLine());
+
+daireAlan = pi * yariCap * yariCap;
+Console.WriteLine("Dairenin alani : {0}", daireAlan);
+
+daireCevre = 2 * pi * yariCap;
+Console.WriteLine("Dairenin cevresi : {0}", daireCevre);
